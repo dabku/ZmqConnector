b5507af63a3e6aa437da197aeb0fefb04d2ba8d2
hrsd_user <dabrowski.kuba@gmail.com>
2018-12-04 10:14:39 +0400

    Added tests for server failures

--- connector/test_zmqconnector.py
+++ connector/test_zmqconnector.py
@@ -1,10 +1,30 @@
 import unittest
 import threading
 import time
+import random
+import string
 from connector.ZmqConnector import Client, Server
 
 
-class FunctionalTests(unittest.TestCase):
+class FunctionalTestsBase(unittest.TestCase):
+    str_tests = ['', 'a', '1', '\'', "'", '"', '\n', 'abc', '123', 'x' * (10 ** 6), 'abc\n\t\'\"']
+    num_tests = [1, 0, -1, 0.1, -0.1, 10 ** 6, 0.1 * 10 ** 6]
+    long_msg = 'x' * 10 ** 6
+
+    @classmethod
+    def tearDownClass(cls):
+        time.sleep(1)
+        cls.server.close()
+
+    def send_message(self, msg, retries, timeout):
+        return self.client.send_message(msg, retries=retries, timeout=timeout)
+
+    @staticmethod
+    def random_string(length=1024):
+        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+
+
+class FunctionalTests(FunctionalTestsBase):
     @classmethod
     def setUpClass(cls):
         cls.client = Client(server_port=5556, address='localhost')
@@ -24,18 +44,8 @@ class FunctionalTests(unittest.TestCase):
         cls.server_thread.start()
         time.sleep(1)
 
-    @classmethod
-    def tearDownClass(cls):
-        time.sleep(1)
-        cls.server.close()
-
-    def send_message(self, msg, retries, timeout):
-        return self.client.send_message(msg, retries=retries, timeout=timeout)
-
 
 class FunctionalTestsBasic(FunctionalTests):
-    str_tests = ['', 'a', '1', '\'', "'", '"', '\n', 'abc', '123', 'x' * (10 ** 6), 'abc\n\t\'\"']
-    num_tests = [1, 0, -1, 0.1, -0.1, 10**6, 0.1*10**6]
 
     def test_text(self):
         tests = self.str_tests
@@ -69,4 +79,46 @@ class FunctionalTestsBasic(FunctionalTests):
             self.assertEqual(msg, response['response'])
 
 
+class FunctionalTestsMoodySrv(FunctionalTestsBase):
+    @classmethod
+    def setUpClass(cls):
+        cls.client = Client(server_port=5556, address='localhost')
+        cls.server = Server(server_port=5556)
+        cls.run_tests = True
 
+        def server_worker(server):
+            while True:
+                try:
+                    message = server.receive_message()
+                    if random.random() > 0.75:
+                        time.sleep(1)
+                    server.send_message({'response': message})
+
+                except server.SocketClosed:
+                    pass
+
+        cls.server_thread = threading.Thread(target=server_worker, args=(cls.server,))
+        cls.server_thread.setDaemon(True)
+        cls.server_thread.start()
+        time.sleep(1)
+
+
+class FunctionalTestsFails(FunctionalTestsMoodySrv):
+    def test_server_operation_for_tests(self):
+        fails = 0
+        for i in range(20):
+            try:
+                self.send_message('short', 0, 750)
+            except Client.SendTimeout:
+                fails += 1
+        self.assertGreater(fails, 0)
+
+    def test_retries(self):
+        for i in range(50):
+            msg = self.random_string()
+            response = self.send_message(msg, 10, 500)
+            self.assertEqual(msg, response['response'])
+
+    def test_short_timeout(self):
+        for i in range(50):
+            self.assertRaises(Client.SendTimeout, self.send_message, self.long_msg, 0, 1)
